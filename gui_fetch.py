d769a4de2be9e3293963f02384b06c5998a503d9
viva-who <nastja.klochkova@gmail.com>
2025-06-27 10:21:15 +0300

    colored tree widget

--- gui_fetch.py
+++ gui_fetch.py
@@ -1,14 +1,55 @@
 import sys
+from io import StringIO
+
+from PyQt5 import QtCore
 
 import resources
+import subprocess
 
-from PyQt5.QtCore import Qt
-from PyQt5.QtGui import QBrush, QIcon, QPixmap, QFont
+from PyQt5.QtCore import Qt, QObject
+from PyQt5.QtGui import QBrush, QIcon, QPixmap, QFont, QColor, QTextCursor
 from PyQt5.QtWidgets import QApplication, QMainWindow, QFileDialog, QMdiSubWindow, QPlainTextEdit, \
-    QTreeWidgetItem, QAction
+    QTreeWidgetItem, QAction, QTextEdit
 from modfab_ui import Ui_mainWindow
 from CLaunch import CLaunch
 
+
+# class EmittingStream(QtCore.QObject):
+#
+#     textWritten = QtCore.pyqtSignal(str)
+#
+#     def write(self, text):
+#         self.textWritten.emit(str(text))
+#
+#
+# class QDbgConsole(QTextEdit):
+#     '''
+#     A simple QTextEdit, with a few pre-set attributes and a file-like
+#     interface.
+#     '''
+#     # Feel free to adjust those
+#     WIDTH = 480
+#     HEIGHT = 320
+#
+#     def __init__(self, parent=None):
+#         super(QDbgConsole, self).__init__(parent)
+#
+#         self._buffer = StringIO()
+#
+#         self.setReadOnly(True)
+#
+#     def write(self, msg):
+#         '''Add msg to the console's output, on a new line.'''
+#         self.insertPlainText(msg)
+#         # Autoscroll
+#         self.moveCursor(QTextCursor.End)
+#         self._buffer.write(msg)
+#
+#     def __getattr__(self, attr):
+#         '''
+#         Fall back to the buffer object if an attribute can't be found.
+#         '''
+#         return getattr(self._buffer, attr)
 
 class Window(QMainWindow, Ui_mainWindow):
     def __init__(self, parent=None):
@@ -79,16 +120,20 @@ class Window(QMainWindow, Ui_mainWindow):
                 background-color: #c5e1eb
             }
         QHeaderView::section:last {
-                background-color: #8fbfcf
-            }    
+                background-color: #adadad
+            }
+        QColumnView {
+                background: #b7bbbd     
         '''
         self.treeWidget.setStyleSheet(stylesheet)
         zap = QTreeWidgetItem()
         zap.setText(0, self.zapusk.launch_fn.split('/')[-1])
+        zap.setBackground(1, QBrush(QColor('#d6d6d6')))
         lst_stanok = list(set([st['Stanok'] for st in data]))
         for st in lst_stanok:
             st_child = QTreeWidgetItem()
             st_child.setText(0, f'Станок {st}')
+            st_child.setBackground(1, QBrush(QColor('#d6d6d6')))
             lst_mod_dict = [el for el in data if el['Stanok'] == st]
             mod_dict = {}
             for el in lst_mod_dict:
@@ -104,6 +149,7 @@ class Window(QMainWindow, Ui_mainWindow):
                     var_child.setText(0, f'исп. {value[0]}: ')
                     var_child.setTextAlignment(0, Qt.AlignRight)
                     var_child.setText(1, value[1])
+                    var_child.setBackground(1, QBrush(QColor('#f2f2f2')))
                     mod_child.addChild(var_child)
                 mod_children_list = []
                 mod_value = 0
@@ -113,7 +159,8 @@ class Window(QMainWindow, Ui_mainWindow):
                 for var in mod_children_list:
                     mod_value += int(var.text(1))
                 mod_child.setText(1, str(mod_value))
-                mod_child.setForeground(1, QBrush(Qt.gray))
+                mod_child.setForeground(1, QBrush(Qt.darkGray))
+                mod_child.setBackground(1, QBrush(QColor('#d6d6d6')))
                 st_child.addChild(mod_child)
             zap.addChild(st_child)
         self.treeWidget.addTopLevelItem(zap)
@@ -152,7 +199,6 @@ class Window(QMainWindow, Ui_mainWindow):
     def print_rpt_stanoks(self):
         self.text.clear()
         self.text.insertPlainText(self.zapusk.rpt_stanoks())
-
 
 
 if __name__ == '__main__':
