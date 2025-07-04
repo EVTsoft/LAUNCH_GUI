90442f9816e84cdccbae734d64aa3643f361128b
viva-who <nastja.klochkova@gmail.com>
2025-07-04 12:57:36 +0300

    saving var changes

--- gui_fetch.py
+++ gui_fetch.py
@@ -1,60 +1,23 @@
 import sys
-from cProfile import label
+import os
 from io import StringIO
-from tkinter.filedialog import dialogstates
 
 from PyQt5 import QtCore
 
 import resources
 import subprocess
+import json
 
-from PyQt5.QtCore import Qt, QObject, QSettings, QLocale, QTranslator
+from PyQt5.QtCore import Qt, QObject, QSettings, QLocale, QTranslator, QSize
 from PyQt5.QtGui import QBrush, QIcon, QPixmap, QFont, QColor, QTextCursor
 from PyQt5.QtWidgets import QApplication, QMainWindow, QFileDialog, QMdiSubWindow, QPlainTextEdit, \
     QTreeWidgetItem, QAction, QTextEdit, QMdiArea, QMenu, QDialog, QLabel, QFontDialog, QHBoxLayout, QVBoxLayout, \
-    QPushButton, QWidget, QToolButton, QTreeWidget
+    QPushButton, QWidget, QToolButton, QTreeWidget, QMessageBox
 from modfab_ui import Ui_mainWindow
 from CLaunch import CLaunch
 from CElModule import CElModule
 
-I18N_QT_PATH = '/usr/share/qt/translations/'
 
-# class EmittingStream(QtCore.QObject):
-#
-#     textWritten = QtCore.pyqtSignal(str)
-#
-#     def write(self, text):
-#         self.textWritten.emit(str(text))
-#
-#
-# class QDbgConsole(QTextEdit):
-#     '''
-#     A simple QTextEdit, with a few pre-set attributes and a file-like
-#     interface.
-#     '''
-#     # Feel free to adjust those
-#     WIDTH = 480
-#     HEIGHT = 320
-#
-#     def __init__(self, parent=None):
-#         super(QDbgConsole, self).__init__(parent)
-#
-#         self._buffer = StringIO()
-#
-#         self.setReadOnly(True)
-#
-#     def write(self, msg):
-#         '''Add msg to the console's output, on a new line.'''
-#         self.insertPlainText(msg)
-#         # Autoscroll
-#         self.moveCursor(QTextCursor.End)
-#         self._buffer.write(msg)
-#
-#     def __getattr__(self, attr):
-#         '''
-#         Fall back to the buffer object if an attribute can't be found.
-#         '''
-#         return getattr(self._buffer, attr)
 class ActionButton(QToolButton):
     def __init__(self, parent=None):
         super().__init__(parent)
@@ -87,6 +50,9 @@ class Window(QMainWindow, Ui_mainWindow):
         self.zapusk = None
         self.state = None
         self.report_state = None
+        self.selected_mod = None
+        self.temp_file_name = None
+        self.temp_json = None
         self.launch_file_name = ''
         self.my_treeWidget = MyTreeWidget()
         self.my_treeWidget.setHeaderLabel('')
@@ -121,6 +87,10 @@ class Window(QMainWindow, Ui_mainWindow):
         self.mod_button.setAction(self.mod_action)
         self.pack_type_button.setAction(self.pack_type_action)
         self.stanok_button.setAction(self.stanok_action)
+        self.spec_button.setIconSize(QSize(30, 30))
+        self.mod_button.setIconSize(QSize(30, 30))
+        self.pack_type_button.setIconSize(QSize(30, 30))
+        self.stanok_button.setIconSize(QSize(30, 30))
         self.container = QWidget()
         self.h_layout = QHBoxLayout()
         self.h_layout.addWidget(self.spec_button)
@@ -138,21 +108,13 @@ class Window(QMainWindow, Ui_mainWindow):
         self.toolBar.addSeparator()
         self.toolBar.addSeparator()
         self.toolBar.addWidget(self.container)
-        # self.toolBar.addAction(self.spec_action)
-        # self.toolBar.addAction(self.mod_action)
-        # self.toolBar.addAction(self.pack_type_action)
-        # self.toolBar.addAction(self.stanok_action)
-        # self.toolBar.insertSeparator(self.spec_action)
+        self.save_zapusk_action.setEnabled(False)
         self.move_mod_action.setVisible(False)
         self.spec_label.setVisible(False)
         self.spec_button.setVisible(False)
         self.mod_button.setVisible(False)
         self.pack_type_button.setVisible(False)
         self.stanok_button.setVisible(False)
-        # self.spec_action.setVisible(False)
-        # self.mod_action.setVisible(False)
-        # self.pack_type_action.setVisible(False)
-        # self.stanok_action.setVisible(False)
         self.toolBar.setStyleSheet('QToolBar { spacing: 5px; }')
         self.toolBar.setToolButtonStyle(Qt.ToolButtonTextUnderIcon)
         self.text = QPlainTextEdit()
@@ -169,6 +131,7 @@ class Window(QMainWindow, Ui_mainWindow):
     def connect_signals(self):
         self.action.triggered.connect(self.open_file_selection)
         self.my_treeWidget.itemPressed.connect(self.handle_tree_items)
+        self.my_treeWidget.itemChanged.connect(self.recount_vars)
         self.spec_action.triggered.connect(self.print_rpt)
         self.mod_action.triggered.connect(self.print_rpt_allMS)
         self.pack_type_action.triggered.connect(self.print_rpt_SMD_pack)
@@ -179,7 +142,7 @@ class Window(QMainWindow, Ui_mainWindow):
         self.stanok_button.clicked.connect(self.print_rpt_stanoks)
         # self.add_stanok_action.triggered.connect(self.add_stanok)
         self.change_font_action.triggered.connect(self.change_font)
-        # self.save_zapusk_action.triggered.connect(self.save_zapusk)
+        self.save_zapusk_action.triggered.connect(self.save_zapusk)
         # self.close_zapusk_action.triggered.connect(self.close_zapusk)
 
     def restore_state(self):
@@ -189,6 +152,8 @@ class Window(QMainWindow, Ui_mainWindow):
         window_state = self.settings.value('windowState')
         if window_state:
             self.setWindowState(window_state)
+        self.temp_json = self.settings.value('json')
+        self.temp_file_name = self.settings.value('tempFile')
         self.launch_file_name = self.settings.value('lastOpenedFile')
         if self.launch_file_name:
             subwindow = QMdiSubWindow()
@@ -210,10 +175,18 @@ class Window(QMainWindow, Ui_mainWindow):
                 self.mod_button.setVisible(True)
                 self.pack_type_button.setVisible(True)
                 self.stanok_button.setVisible(True)
-                # self.spec_action.setVisible(True)
-                # self.mod_action.setVisible(True)
-                # self.pack_type_action.setVisible(True)
-                # self.stanok_action.setVisible(True)
+            elif self.state == 'Режим работы с модулями':
+                self.selected_mod = self.settings.value('lastSelectedModule')
+                if self.selected_mod:
+                    self.mdiArea.activeSubWindow().setWidget(self.text)
+                    self.spec_label.setVisible(False)
+                    self.spec_button.setVisible(False)
+                    self.mod_button.setVisible(False)
+                    self.pack_type_button.setVisible(False)
+                    self.stanok_button.setVisible(False)
+                    self.move_mod_action.setVisible(True)
+                    self.text.clear()
+                    self.text.insertPlainText(self.selected_mod.report())
         self.report_state = self.settings.value('lastReportState')
         if self.report_state:
             if self.report_state == 'zapusk':
@@ -225,15 +198,6 @@ class Window(QMainWindow, Ui_mainWindow):
             elif self.report_state == 'stanoks':
                 self.print_rpt_stanoks()
             self.mdiArea.activeSubWindow().setWidget(self.text)
-
-    # def contextMenuEvent(self, event):
-    #     menu = QMenu(self.centralwidget)
-    #     # if '.zap' in self.treeWidget.itemAt(event.globalPos()).text(0):
-    #     add_stanok_action = QAction('Добавить станок', self)
-    #     close_action = QAction('Закрыть запуск', self)
-    #     menu.addAction(add_stanok_action)
-    #     menu.addAction(close_action)
-    #     menu.exec(event.globalPos())
 
     def open_file_selection(self):
         dialog = QFileDialog()
@@ -251,6 +215,17 @@ class Window(QMainWindow, Ui_mainWindow):
         subwindow.setGeometry(0, 0, 887, 671)
         subwindow.showMaximized()
         self.zapusk = CLaunch(self.launch_file_name)
+        parts = self.launch_file_name.split('.')
+        parts.insert(1, '_tmp.')
+        self.temp_file_name = ''.join(parts)
+        new_file = open(self.temp_file_name, 'w+', encoding='utf-16')
+        with open(self.launch_file_name, 'r', encoding='utf-16') as file:
+            lines = file.readlines()
+            file.close()
+        new_file.write(''.join(lines))
+        self.temp_json = json.loads(''.join(lines))
+        print(self.temp_json)
+        new_file.close()
         self.launch_tree()
 
     def launch_tree(self):
@@ -278,11 +253,13 @@ class Window(QMainWindow, Ui_mainWindow):
         zap = QTreeWidgetItem()
         zap.setText(0, self.zapusk.launch_fn.split('/')[-1])
         zap.setBackground(1, QBrush(QColor('#d6d6d6')))
+        zap.setFlags(Qt.ItemIsEnabled | Qt.ItemIsSelectable)
         lst_stanok = list(set([st['Stanok'] for st in data]))
         for st in lst_stanok:
             st_child = QTreeWidgetItem()
             st_child.setText(0, f'Станок {st}')
             st_child.setBackground(1, QBrush(QColor('#d6d6d6')))
+            st_child.setFlags(Qt.ItemIsEnabled | Qt.ItemIsSelectable | Qt.ItemIsDropEnabled)
             lst_mod_dict = [el for el in data if el['Stanok'] == st]
             mod_dict = {}
             for el in lst_mod_dict:
@@ -299,6 +276,7 @@ class Window(QMainWindow, Ui_mainWindow):
                     var_child.setTextAlignment(0, Qt.AlignRight)
                     var_child.setText(1, value[1])
                     var_child.setBackground(1, QBrush(QColor('#f2f2f2')))
+                    var_child.setFlags(Qt.ItemIsEnabled | Qt.ItemIsSelectable | Qt.ItemIsEditable)
                     mod_child.addChild(var_child)
                 mod_children_list = []
                 mod_value = 0
@@ -310,6 +288,7 @@ class Window(QMainWindow, Ui_mainWindow):
                 mod_child.setText(1, str(mod_value))
                 mod_child.setForeground(1, QBrush(Qt.darkGray))
                 mod_child.setBackground(1, QBrush(QColor('#d6d6d6')))
+                mod_child.setFlags((Qt.ItemIsSelectable | Qt.ItemIsDragEnabled | Qt.ItemIsEnabled) & ~Qt.ItemIsDropEnabled)
                 st_child.addChild(mod_child)
             zap.addChild(st_child)
         self.my_treeWidget.addTopLevelItem(zap)
@@ -333,16 +312,16 @@ class Window(QMainWindow, Ui_mainWindow):
             self.mod_button.setVisible(True)
             self.pack_type_button.setVisible(True)
             self.stanok_button.setVisible(True)
-            # self.spec_action.setVisible(True)
-            # self.mod_action.setVisible(True)
-            # self.pack_type_action.setVisible(True)
-            # self.stanok_action.setVisible(True)
+            print(self.spec_button.size())
+            print(self.mod_button.size())
+            print(self.pack_type_button.size())
+            print(self.stanok_button.size())
             if self.report_state is None:
                 self.text.clear()
                 self.text.insertPlainText(self.zapusk.rpt())
-
         elif 'Станок' in item.parent().text(0):
             self.state = 'Режим работы с модулями'
+            self.report_state = None
             self.statusbar.showMessage(self.state)
             self.mdiArea.activeSubWindow().setWidget(self.text)
             self.spec_label.setVisible(False)
@@ -351,9 +330,10 @@ class Window(QMainWindow, Ui_mainWindow):
             self.pack_type_button.setVisible(False)
             self.stanok_button.setVisible(False)
             self.move_mod_action.setVisible(True)
-            mod = CElModule(item.text(0), 'launch')
+            print(item.parent().indexOfChild(item))
+            self.selected_mod = CElModule(item.text(0), 'launch')
             self.text.clear()
-            self.text.insertPlainText(mod.report())
+            self.text.insertPlainText(self.selected_mod.report())
 
     def print_rpt(self):
         self.report_state = 'zapusk'
@@ -382,23 +362,97 @@ class Window(QMainWindow, Ui_mainWindow):
         if ok:
             self.text.setFont(font)
 
+    def recount_vars(self, item):
+        self.save_zapusk_action.setEnabled(True)
+        mod = item.parent()
+        total = int(mod.text(1))
+        summa = 0
+        for i in range(mod.childCount()):
+            var = mod.child(i)
+            summa += int(var.text(1))
+        diff = total - summa
+        try:
+            new_value = int(mod.child(mod.indexOfChild(item) + 1).text(1)) + diff
+            mod.child(mod.indexOfChild(item) + 1).setText(1, str(new_value))
+        except Exception:
+            new_value = int(mod.child(mod.indexOfChild(item) - 1).text(1)) + diff
+            mod.child(mod.indexOfChild(item) - 1).setText(1, str(new_value))
+        needed_index = 0
+        for item in self.temp_json:
+            if item['bommod'] == mod.text(0):
+                needed_index = self.temp_json.index(item)
+                break
+        inc = 0
+        for var in self.temp_json[needed_index]['mtypes']:
+            var['quantity'] = mod.child(inc).text(1)
+            inc += 1
+        with open(self.temp_file_name, 'w+', encoding='utf-16') as temp_file:
+            s = json.dumps(self.temp_json, indent=0)
+            temp_file.write(s)
+            temp_file.close()
+
+    def save_zapusk(self):
+        q = QMessageBox()
+        q.setWindowTitle('Сохранение')
+        q.setText(f'Сохранить изменения в файле {self.launch_file_name}?')
+        q.setStandardButtons(QMessageBox.Ok | QMessageBox.Cancel)
+        q.setIcon(QMessageBox.Question)
+        button = q.exec()
+        if button == QMessageBox.Ok:
+            with open(self.temp_file_name, 'r', encoding='utf-16') as temp_file:
+                with open(self.launch_file_name, 'w', encoding='utf-16') as file:
+                    lines = temp_file.readlines()
+                    file.write(''.join(lines))
+                    file.close()
+                    temp_file.close()
+            self.zapusk = CLaunch(self.launch_file_name)
+            self.text.clear()
+            self.state = 'Режим работы с запуском'
+            self.statusbar.showMessage(self.state)
+            if self.report_state:
+                if self.report_state == 'zapusk':
+                    self.text.insertPlainText(self.zapusk.rpt())
+                elif self.report_state == 'mod':
+                    self.text.insertPlainText(self.zapusk.rpt_allMS())
+                elif self.report_state == 'pack_type':
+                    self.text.insertPlainText(self.zapusk.rpt_SMD_pack())
+                elif self.report_state == 'stanoks':
+                    self.text.insertPlainText(self.zapusk.rpt_stanoks())
+            else:
+                self.report_state = 'zapusk'
+                self.text.insertPlainText(self.zapusk.rpt())
+            self.save_zapusk_action.setEnabled(False)
+
     def closeEvent(self, event):
+        if self.save_zapusk_action.isEnabled():
+            save_dlg = QMessageBox()
+            save_dlg.setWindowTitle('Закрыть запуск')
+            save_dlg.setText(f'Сохранить несохранённые изменения в файле {self.launch_file_name} перед закрытием?')
+            save_dlg.setIcon(QMessageBox.Question)
+            save_dlg.setStandardButtons(QMessageBox.Save | QMessageBox.Close)
+            button = save_dlg.exec()
+            if button == QMessageBox.Save:
+                with open(self.temp_file_name, 'r', encoding='utf-16') as temp_file:
+                    with open(self.launch_file_name, 'w', encoding='utf-16') as file:
+                        lines = temp_file.readlines()
+                        file.write(''.join(lines))
+                        file.close()
+                        temp_file.close()
+                self.zapusk = CLaunch(self.launch_file_name)
         self.settings.setValue('windowGeometry', self.geometry())
         self.settings.setValue('windowState', self.windowState())
         self.settings.setValue('lastOpenedFile', self.launch_file_name)
         self.settings.setValue('lastState', self.state)
         self.settings.setValue('lastReportState', self.report_state)
+        self.settings.setValue('lastSelectedModule', self.selected_mod)
+        self.settings.setValue('json', self.temp_json)
+        self.settings.setValue('tempFile', self.temp_file_name)
         self.settings.sync()
         super().closeEvent(event)
 
+
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    locale = QLocale.system().name()
-    translator = QTranslator(app)
-    translator.load('{}qtbase_{}.qm'.format(I18N_QT_PATH, locale))
-    app.installTranslator(translator)
     w = Window()
     w.show()
     sys.exit(app.exec_())
-
-
